c9cfa9a3a26650908c667c0a7e07a87f17e72f8d
wandl-6A72h <hiwepy@gmail.com>
2026-08-06 14:59:43 +0800

    chore: 注释作者信息统一为 @Loong Wan

--- src/main/java/org/bytedeco/opencv/spring/boot/OpenCVFaceRecognitionTemplate.java
+++ src/main/java/org/bytedeco/opencv/spring/boot/OpenCVFaceRecognitionTemplate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ * Copyright (c) 2018, Loong Wan (https://github.com/loong10k).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -42,7 +42,7 @@ import com.alibaba.fastjson.JSONObject;
 /**
  * OpenCV face detection and image comparison template.
  *
- * @author ： <a href="https://github.com/hiwepy">wandl</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class OpenCVFaceRecognitionTemplate {
 
